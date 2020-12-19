1d58eaf477a172fb79de418e0f6dbf25d051c0ea
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-12-19 08:35:59 -0600

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import MoviesPage from './MoviesPage';
 
 class App extends Component {
 
-  state = {
-    movies: {
-      1: { id: 1, title: 'A River Runs Through It' },
-      2: { id: 2, title: 'Se7en' },
-      3: { id: 3, title: 'Inception' }
-    }
-  }
+  state = { movies: { 1: { id: 1, title: 'A River Runs Through It' }, 2: { id: 2, title: 'Se7en' }, 3: { id: 3, title: 'Inception' } } }
 
   render() {
     return (
